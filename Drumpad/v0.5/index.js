89bf0996f060f58385fec9805dca162e2c50ee58
PawelSzczepaniak99 <61648162+PawelSzczepaniak99@users.noreply.github.com>
2021-04-03 18:50:49 +0200

    Add files via upload

---
+++ Drumpad/v0.5/index.js
@@ -0,0 +1,247 @@
+var clapSound;
+var kickSound;
+var snareSound;
+var hihatSound;
+var openhatSound;
+var rideSound;
+var boomSound;
+var tinkSound;
+var tomSound;
+var padBomm = document.getElementById("padBoom");
+var padClap = document.getElementById("padClap");
+var padKick = document.getElementById("padKick");
+var padSanare = document.getElementById("padSnare");
+var padHihat = document.getElementById("padHihat");
+var padOpenhat = document.getElementById("padOpenhat");
+var padRide = document.getElementById("padRide");
+var padTink = document.getElementById("padTink");
+var padTom = document.getElementById("padTom");
+var recordButton1 = document.getElementById("channel1Play");
+var recordButton2 = document.getElementById("channel2Play");
+var recordButton3 = document.getElementById("channel3Play");
+var recordButton4 = document.getElementById("channel4Play");
+var btnPlay1 = document.getElementById("btnPlay1");
+var btnPlay2 = document.getElementById("btnPlay2");
+var btnPlay3 = document.getElementById("btnPlay3");
+var btnPlay4 = document.getElementById("btnPlay4");
+var resetBtn1 = document.getElementById("resetBtn1");
+var isRec = false;
+var isRec2 = false;
+var isRec3 = false;
+var isRec4 = false;
+var channel1 = [];
+var channel2 = [];
+var channel3 = [];
+var channel4 = [];
+appStart();
+function appStart() {
+    document.addEventListener('keypress', onKeyDown);
+    var btnChannel1Play = document.querySelector('#channel1Play');
+    btnPlay1.addEventListener('click', onChannel1Play);
+    getAudioElements();
+    var btnChannel2Play = document.querySelector('#channel2Play');
+    btnPlay2.addEventListener('click', onChannel2Play);
+    var btnChannel3Play = document.querySelector('#channel3Play');
+    btnPlay3.addEventListener('click', onChannel3Play);
+    var btnChannel4Play = document.querySelector('#channel4Play');
+    btnPlay4.addEventListener('click', onChannel4Play);
+}
+function onChannel1Play() {
+    channel1.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.time);
+    });
+}
+function onChannel2Play() {
+    channel2.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.time);
+    });
+}
+function onChannel3Play() {
+    channel3.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.time);
+    });
+}
+function onChannel4Play() {
+    channel4.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.time);
+    });
+}
+function getAudioElements() {
+    clapSound = document.querySelector('[data-sound="clap"]');
+    kickSound = document.querySelector('[data-sound="kick"]');
+    snareSound = document.querySelector('[data-sound="snare"]');
+    hihatSound = document.querySelector('[data-sound="hihat"]');
+    openhatSound = document.querySelector('[data-sound="openhat"]');
+    rideSound = document.querySelector('[data-sound="ride"]');
+    boomSound = document.querySelector('[data-sound="boom"]');
+    tinkSound = document.querySelector('[data-sound="tink"]');
+    tomSound = document.querySelector('[data-sound="tom"]');
+}
+function onKeyDown(ev) {
+    var key = ev.key;
+    var time = ev.timeStamp;
+    if (isRec) {
+        channel1.push({ key: key, time: time });
+        playSound(key);
+    }
+    else {
+        playSound(key);
+    }
+    if (isRec2) {
+        channel2.push({ key: key, time: time });
+        playSound(key);
+    }
+    else {
+        playSound(key);
+    }
+    if (isRec3) {
+        channel3.push({ key: key, time: time });
+        playSound(key);
+    }
+    else {
+        playSound(key);
+    }
+    if (isRec4) {
+        channel4.push({ key: key, time: time });
+        playSound(key);
+    }
+    else {
+        playSound(key);
+    }
+}
+function playSound(key) {
+    switch (key) {
+        case 'a':
+            clapSound.currentTime = 0;
+            clapSound.play();
+            break;
+        case 'b':
+            kickSound.currentTime = 0;
+            kickSound.play();
+            break;
+        case 's':
+            snareSound.currentTime = 0;
+            snareSound.play();
+            break;
+        case 'h':
+            hihatSound.currentTime = 0;
+            hihatSound.play();
+            break;
+        case 'o':
+            openhatSound.currentTime = 0;
+            openhatSound.play();
+            break;
+        case 'r':
+            rideSound.currentTime = 0;
+            rideSound.play();
+            break;
+        case 'n':
+            boomSound.currentTime = 0;
+            boomSound.play();
+            break;
+        case 'm':
+            tinkSound.currentTime = 0;
+            tinkSound.play();
+            break;
+        case 'v':
+            tomSound.currentTime = 0;
+            tomSound.play();
+            break;
+        default:
+            alert("Tutaj nie ma żadnego dzwięku!");
+            break;
+    }
+}
+padBomm.addEventListener('click', function () {
+    boomSound.play();
+    console.log("padboom");
+    boomSound.currentTime = 0;
+});
+padClap.addEventListener('click', function () {
+    clapSound.play();
+    clapSound.currentTime = 0;
+});
+padKick.addEventListener('click', function () {
+    kickSound.play();
+    kickSound.currentTime = 0;
+});
+padSanare.addEventListener('click', function () {
+    snareSound.play();
+    snareSound.currentTime = 0;
+});
+padHihat.addEventListener('click', function () {
+    hihatSound.play();
+    hihatSound.currentTime = 0;
+});
+padOpenhat.addEventListener('click', function () {
+    openhatSound.play();
+    openhatSound.currentTime = 0;
+});
+padRide.addEventListener('click', function () {
+    rideSound.play();
+    rideSound.currentTime = 0;
+});
+padTink.addEventListener('click', function () {
+    tinkSound.play();
+    tinkSound.currentTime = 0;
+});
+padTom.addEventListener('click', function () {
+    tomSound.play();
+    tomSound.currentTime = 0;
+});
+recordButton1.addEventListener("click", function () {
+    if (!isRec) {
+        recordButton1.classList.add("rec");
+        isRec = true;
+        recordButton1.textContent = "Recording";
+    }
+    else if (isRec) {
+        recordButton1.classList.remove("rec");
+        isRec = false;
+        recordButton1.textContent = "Record channel 1";
+    }
+});
+recordButton2.addEventListener("click", function () {
+    if (!isRec2) {
+        recordButton2.classList.add("rec");
+        isRec2 = true;
+        recordButton2.textContent = "Recording";
+    }
+    else if (isRec2) {
+        recordButton2.classList.remove("rec");
+        isRec2 = false;
+        recordButton2.textContent = "Record channel 2";
+    }
+});
+recordButton3.addEventListener("click", function () {
+    if (!isRec3) {
+        recordButton3.classList.add("rec");
+        isRec3 = true;
+        recordButton3.textContent = "Recording";
+    }
+    else if (isRec3) {
+        recordButton3.classList.remove("rec");
+        isRec = false;
+        recordButton3.textContent = "Record channel 3";
+    }
+});
+recordButton4.addEventListener("click", function () {
+    if (!isRec4) {
+        recordButton4.classList.add("rec");
+        isRec4 = true;
+        recordButton4.textContent = "Recording";
+    }
+    else if (isRec4) {
+        recordButton4.classList.remove("rec");
+        isRec4 = false;
+        recordButton4.textContent = "Record channel 4";
+    }
+});
+resetBtn1.addEventListener('click', function () {
+    for (var i = 0; i < channel1.length; i) {
+        channel1.shift();
+        channel2.shift();
+        channel3.shift();
+        channel4.shift();
+    }
+});
---
+++ Drumpad/v0.5/index.ts
@@ -0,0 +1,311 @@
+let clapSound: HTMLAudioElement;
+let kickSound: HTMLAudioElement;
+let snareSound: HTMLAudioElement;
+let hihatSound: HTMLAudioElement;
+let openhatSound: HTMLAudioElement;
+let rideSound: HTMLAudioElement;
+let boomSound: HTMLAudioElement;
+let tinkSound: HTMLAudioElement;
+let tomSound: HTMLAudioElement;
+
+let padBomm = document.getElementById("padBoom");
+let padClap = document.getElementById("padClap");
+let padKick = document.getElementById("padKick");
+let padSanare = document.getElementById("padSnare");
+let padHihat = document.getElementById("padHihat");
+let padOpenhat = document.getElementById("padOpenhat");
+let padRide = document.getElementById("padRide");
+let padTink = document.getElementById("padTink");
+let padTom = document.getElementById("padTom");
+
+let recordButton1 = document.getElementById("channel1Play");
+let recordButton2 = document.getElementById("channel2Play");
+let recordButton3 = document.getElementById("channel3Play");
+let recordButton4 = document.getElementById("channel4Play");
+
+let btnPlay1 = document.getElementById("btnPlay1")
+let btnPlay2 = document.getElementById("btnPlay2")
+let btnPlay3 = document.getElementById("btnPlay3")
+let btnPlay4 = document.getElementById("btnPlay4")
+
+let resetBtn1 = document.getElementById("resetBtn1")
+
+let isRec = false;
+let isRec2 = false;
+let isRec3 = false;
+let isRec4 = false;
+
+
+
+const channel1: any[] = [];
+const channel2: any[] = [];
+const channel3: any[] = [];
+const channel4: any[] = [];
+
+appStart();
+
+function appStart(): void {
+    document.addEventListener('keypress', onKeyDown);
+    const btnChannel1Play = document.querySelector('#channel1Play')
+    btnPlay1.addEventListener('click', onChannel1Play);
+    getAudioElements();
+
+    const btnChannel2Play = document.querySelector('#channel2Play');
+    btnPlay2.addEventListener('click', onChannel2Play);
+
+    const btnChannel3Play = document.querySelector('#channel3Play');
+    btnPlay3.addEventListener('click', onChannel3Play);
+
+    const btnChannel4Play = document.querySelector('#channel4Play')
+    btnPlay4.addEventListener('click', onChannel4Play)
+    
+    
+}
+function onChannel1Play(): void {
+    channel1.forEach(sound => {
+        setTimeout(() => playSound(sound.key), sound.time)
+
+        })
+        
+    
+}
+function onChannel2Play(): void {
+    channel2.forEach(sound => {
+        setTimeout(() => playSound(sound.key), sound.time)
+
+        })
+        
+    
+}
+function onChannel3Play() {
+    channel3.forEach(sound => {
+        setTimeout(() => playSound(sound.key), sound.time)
+    })
+    
+}
+function onChannel4Play(){
+    channel4.forEach(sound => {
+        setTimeout(() => playSound(sound.key), sound.time)
+    })    
+}
+
+
+function getAudioElements(){
+    clapSound = document.querySelector('[data-sound="clap"]')
+    kickSound = document.querySelector('[data-sound="kick"]')
+    snareSound = document.querySelector('[data-sound="snare"]')
+    hihatSound = document.querySelector('[data-sound="hihat"]')
+    openhatSound = document.querySelector('[data-sound="openhat"]')
+    rideSound = document.querySelector('[data-sound="ride"]')
+    boomSound = document.querySelector('[data-sound="boom"]')
+    tinkSound = document.querySelector('[data-sound="tink"]')
+    tomSound = document.querySelector('[data-sound="tom"]')
+}
+
+function onKeyDown(ev: KeyboardEvent): void {
+    const key = ev.key;
+    const time = ev.timeStamp;
+
+    if(isRec)
+    {
+    channel1.push({key, time})
+    playSound(key)
+    }
+    else
+    {
+        playSound(key)
+    }
+
+    if(isRec2)
+    {
+    channel2.push({key, time})
+    playSound(key)
+    }
+    else
+    {
+        playSound(key)
+    }
+
+
+    if(isRec3)
+    {
+    channel3.push({key, time})
+    playSound(key)
+    }
+    else
+    {
+        playSound(key)
+    }
+
+    if(isRec4)
+    {
+    channel4.push({key, time})
+    playSound(key)
+    }
+    else
+    {
+        playSound(key)
+    }
+}
+
+function playSound(key: string): void {
+    switch (key) {
+        case 'a':
+            clapSound.currentTime = 0;
+            clapSound.play();
+            break;
+        case 'b':
+            kickSound.currentTime = 0;
+            kickSound.play();
+            break;
+        case 's':
+            snareSound.currentTime = 0;
+            snareSound.play();
+            break;
+        case 'h':
+            hihatSound.currentTime = 0;
+            hihatSound.play();
+            break;
+        case 'o':
+            openhatSound.currentTime = 0;
+            openhatSound.play();
+            break;
+        case 'r':
+            rideSound.currentTime = 0;
+            rideSound.play();
+            break;
+        case 'n':
+            boomSound.currentTime = 0;
+            boomSound.play();
+            break;
+        case 'm':
+            tinkSound.currentTime = 0;
+            tinkSound.play();
+            break;
+        case 'v':
+            tomSound.currentTime = 0;
+            tomSound.play();
+            break;
+        default:
+            alert("Tutaj nie ma żadnego dzwięku!")
+            break;
+    }
+
+   
+}
+
+
+padBomm.addEventListener('click', function(){
+    boomSound.play();
+    console.log("padboom");
+     boomSound.currentTime = 0;
+    })
+
+padClap.addEventListener('click', function(){
+    clapSound.play()
+    clapSound.currentTime = 0;
+})
+padKick.addEventListener('click', function(){
+    kickSound.play();
+    kickSound.currentTime = 0;
+})
+padSanare.addEventListener('click', function(){
+    snareSound.play();
+    snareSound.currentTime = 0;
+})
+padHihat.addEventListener('click', function(){
+    hihatSound.play();
+    hihatSound.currentTime = 0;
+})
+padOpenhat.addEventListener('click', function (){
+    openhatSound.play()
+    openhatSound.currentTime = 0;
+    
+})
+padRide.addEventListener('click', function(){
+    rideSound.play()
+    rideSound.currentTime = 0;
+})
+padTink.addEventListener('click', function() {
+    tinkSound.play()
+    tinkSound.currentTime = 0;
+    
+})
+padTom.addEventListener('click', function(){
+    tomSound.play()
+    tomSound.currentTime = 0;
+})
+
+recordButton1.addEventListener("click", function () {
+    if(!isRec)
+    {
+        recordButton1.classList.add("rec")
+        isRec = true;
+        recordButton1.textContent = "Recording"
+
+    }
+    else if(isRec)
+    {
+        recordButton1.classList.remove("rec")
+        isRec = false;
+        recordButton1.textContent = "Record channel 1"
+    }
+} )
+
+recordButton2.addEventListener("click", function () {
+    if(!isRec2)
+    {
+        recordButton2.classList.add("rec")
+        isRec2 = true;
+        recordButton2.textContent = "Recording"
+
+    }
+    else if(isRec2)
+    {
+        recordButton2.classList.remove("rec")
+        isRec2 = false;
+        recordButton2.textContent = "Record channel 2"
+    }
+} )
+
+recordButton3.addEventListener("click", function() {
+    if(!isRec3)
+    {
+        recordButton3.classList.add("rec")
+        isRec3 = true;
+        recordButton3.textContent = "Recording"
+    }
+    else if(isRec3)
+    {
+        recordButton3.classList.remove("rec")
+        isRec = false;
+        recordButton3.textContent = "Record channel 3"
+    }
+    
+})
+
+recordButton4.addEventListener("click", function() {
+    if(!isRec4)
+    {
+        recordButton4.classList.add("rec")
+        isRec4 = true;
+        recordButton4.textContent = "Recording"
+    }
+    else if(isRec4)
+    {
+        recordButton4.classList.remove("rec")
+        isRec4 = false;
+        recordButton4.textContent = "Record channel 4"
+    }
+
+})
+
+resetBtn1.addEventListener('click', function () {
+    
+    for (let i = 0; i < channel1.length; i) {
+        channel1.shift();
+        channel2.shift();
+        channel3.shift();
+        channel4.shift();
+    }
+})
