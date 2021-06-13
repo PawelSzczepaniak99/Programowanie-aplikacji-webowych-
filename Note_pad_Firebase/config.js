e27773ad366f715568d48a62fe5584c37d3375e4
PawelSzczepaniak99 <61648162+PawelSzczepaniak99@users.noreply.github.com>
2021-06-13 14:44:28 +0200

    Add files via upload

---
+++ Note_pad_Firebase/config.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.firebaseConfig = void 0;
+exports.firebaseConfig = {
+    apiKey: "AIzaSyDdUVyhndesmBdWChPMwk2Rar58SIe2h58",
+    authDomain: "notepad-8263e.firebaseapp.com",
+    projectId: "notepad-8263e",
+    storageBucket: "notepad-8263e.appspot.com",
+    messagingSenderId: "520607171850",
+    appId: "1:520607171850:web:8a136cff0ad514f25e66f3"
+};
